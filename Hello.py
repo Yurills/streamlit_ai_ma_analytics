e134aceea1a054ae34126d8af163f28e2971c27f
Yurills <rawityurill@gmail.com>
2025-08-22 17:56:28 +0700

    fix folder issues

--- Hello.py
+++ Hello.py
@@ -14,9 +14,7 @@ st.title("📊 Data Dashboard")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    
+    ## Welcome to the AI-Powered Company Financial Dashboard!
     **👈 Select workflows from the sidebar** to see some examples
     ### What our AI can currently do:
     - **AI Analysis**: Get insights and PDFs report from comparing two companies
